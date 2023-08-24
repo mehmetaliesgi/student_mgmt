1f3274213120238dee83fe115cdd979f089f5b55
mehmetaliesgi <mehmetaliesgi@gmail.comgit config --global user.name mehmetaliesgi>
2023-08-24 22:52:35 +0300

    scaffold learned and created course with scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'welcome#index'
   resources :students
+  resources :courses
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_191703) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_194408) do
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
