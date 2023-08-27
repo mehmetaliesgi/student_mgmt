671e4d9d92ba941ba29751aa028503d68bc46136
mehmetaliesgi <mehmetaliesgi@gmail.comgit config --global user.name mehmetaliesgi>
2023-08-27 23:39:08 +0300

    ActiveRecord collbacks researched and understood. A little example writed. If you want to more details you can visit ROR page.

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,15 @@ class Student < ApplicationRecord
   has_many :projects, through: :student_projects 
 
   validates :first_name, :last_name, :email, presence: true
+
+  after_create :display_student_age
+  def display_student_age
+    if self.date_of_birth.present?
+      age = Date.today.year - self.date_of_birth.year
+      puts "========= Age of student is #{age} =============="
+    else
+      puts "Age can not be calculated without date_of_birth"
+    end
+  end
+
 end
