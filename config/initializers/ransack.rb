ec79183d1967c4ce6d953421871c006dc5d3e313
mehmetaliesgi <mehmetaliesgi@gmail.comgit config --global user.name mehmetaliesgi>
2023-09-10 15:32:18 +0300

    student index edited and added filters

---
+++ config/initializers/ransack.rb
@@ -0,0 +1,6 @@
+Ransack.configure do |c|
+    c.custom_arrows = {
+        up_arrow: '<i class="fa-solid fa-arrow-down-a-z"></i>',
+        down_arrow: '<i class="fa-solid fa-arrow-up-z-a"></i>'  
+    }
+  end
