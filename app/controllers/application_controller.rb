e19dfcca6ae166deae7403aeabca40ec01361bca
mehmetaliesgi <mehmetaliesgi@gmail.comgit config --global user.name mehmetaliesgi>
2023-09-16 19:14:53 +0300

    name and lastname attributes added to devise attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :print_display_message
   after_action :after_display_message
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def print_display_message
     puts "I am a inherited before action filter"
@@ -8,5 +9,13 @@ class ApplicationController < ActionController::Base
 
   def after_display_message
     puts "I am a inherited after action filter"
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :lastname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname])
   end
 end
