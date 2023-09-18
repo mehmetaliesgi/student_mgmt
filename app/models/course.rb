ad11f7d6d461ef23e1f58b1238ed233624383a33
mehmetaliesgi <mehmetaliesgi@gmail.comgit config --global user.name mehmetaliesgi>
2023-09-19 01:14:20 +0300

    Actiontext Trix added the project

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
   include Validatable
 
   has_and_belongs_to_many :students
+  has_rich_text :content
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "description", "id", "name", "updated_at"]
